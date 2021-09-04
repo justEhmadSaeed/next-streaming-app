2147f73d2c7fa49cd2af215cf2f3c262c9bff0c6
Ehmad <justehmadsaeed@gmail.com>
2021-09-05 01:31:45 +0500

    📦 NEW: Header Item

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import HeaderItem from './HeaderItem';
 import {
 	HomeIcon,
 	CollectionIcon,
@@ -12,7 +13,12 @@ const Header = () => {
 	return (
 		<header className='flex flex-col sm:flex-row m-5 justify-between items-center h-auto'>
 			<div className='flex flex-grow justify-evenly max-w-2xl'>
-				
+				<HeaderItem title='HOME' Icon={HomeIcon} />
+				<HeaderItem title='TRENDING' Icon={LightningBoltIcon} />
+				<HeaderItem title='VERIFIED' Icon={BadgeCheckIcon} />
+				<HeaderItem title='COLLECTIONS' Icon={CollectionIcon} />
+				<HeaderItem title='SEARCH' Icon={SearchIcon} />
+				<HeaderItem title='ACCOUNT' Icon={UserIcon} />
 			</div>
 			<Image
 				className='object-contain'
