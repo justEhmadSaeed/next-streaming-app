ae1d52f775292ea62f7ae9a2a0fc01900d899e51
Ehmad <justehmadsaeed@gmail.com>
2021-09-05 13:26:27 +0500

    📦 NEW: Navbar

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import Header from '../components/Header';
+import Navbar from '../components/Navbar';
 
 export default function Home() {
 	return (
@@ -15,6 +16,7 @@ export default function Home() {
 			</Head>
 
 			<Header />
+			<Navbar />
 		</div>
 	);
 }
