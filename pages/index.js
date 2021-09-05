b2dbb85918a49938e306e51d708fd8a5531b9df4
Ehmad <justehmadsaeed@gmail.com>
2021-09-05 20:26:00 +0500

    📦 NEW: getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,26 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import Header from '../components/Header';
 import Navbar from '../components/Navbar';
+import requests from '../utils/requests';
 
-export default function Home() {
+export const getServerSideProps = async (context) => {
+	const genre = context.query.genre;
+	const request = await fetch(
+		`https://api.themoviedb.org/3${
+			requests[genre]?.url || requests.fetchTrending.url
+		}`
+	)
+		.then((res) => res.json())
+		.catch((err) => console.log(err));
+
+	return {
+		props: {
+			results: request.results,
+		},
+	};
+};
+
+export default function Home({results}) {
 	return (
 		<div>
 			<Head>
@@ -20,3 +37,4 @@ export default function Home() {
 		</div>
 	);
 }
+
